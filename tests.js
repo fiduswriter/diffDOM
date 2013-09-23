c6d37375f83cf8300d1031f939fdef78a146ba87
Mark Aronin <steroidcat@gmail.com>
2013-09-23 17:55:22 -0400

    fixed some typos

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 /*	True false testing
 	Test for values returned from functions
 */
-test( "true/False Test", function() {
+test( "true/False Test examples", function() {
 	//ok( truthy [, message ] )
 	ok(true , "True passes Test");
 	ok(1, "Any number but 0 passes test");
@@ -13,12 +13,12 @@ test( "true/False Test", function() {
 /* 	Assertion testing (Comparison)
 	Tests for actual and expected arguments
 */
-test( "Assertion Test", function() {
+test( "Assertion Test examples", function() {
 	//equal( actual, expected [, message ] )
  	equal( 5, 5, "Numbers are equal, test passed" );
   	equal( "", 0, "Empty, Zero; equal succeeds" );
-  	equal( "<ol><li>1</li><ol>", "<ol><li>1</li><ol>" , "Are equal, tes passed");
-  	equal( "<ol><li>1</li><ol>", "<ol><li>2</li><ol>" , "Are not equal, tes failed");
+  	equal( "<ol><li>1</li><ol>", "<ol><li>1</li><ol>" , "Are equal, test passed");
+  	equal( "<ol><li>1</li><ol>", "<ol><li>2</li><ol>" , "Are not equal, test failed");
   
 });
 
