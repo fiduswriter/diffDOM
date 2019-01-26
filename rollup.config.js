6976e1d3cf2762f7b2d1c3e8784953ee1ac9c91d
Johannes Wilm <mail@johanneswilm.org>
2019-01-26 23:18:12 +0100

    add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,5 @@
-//import commonjs from 'rollup-plugin-commonjs'
-//import globals from 'rollup-plugin-node-globals'
-//import builtins from 'rollup-plugin-node-builtins'
-//import resolve from 'rollup-plugin-node-resolve'
-//import json from 'rollup-plugin-json'
 import buble from 'rollup-plugin-buble'
-//import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
     input: 'src/index.js',
@@ -20,12 +15,7 @@ export default {
         },
     ],
     plugins: [
-//        commonjs(),
-//        globals(),
-//        builtins(),
-//        resolve(),
-//        json(),
         buble(),
-//        terser()
+        terser()
     ]
 }
