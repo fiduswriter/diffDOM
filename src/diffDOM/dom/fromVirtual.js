a93d3dd7c9e988f24a767e392b499926faa69bb4
Johannes Wilm <mail@johanneswilm.org>
2019-01-27 13:49:00 +0100

    refactor

---
+++ src/diffDOM/dom/fromVirtual.js
@@ -0,0 +1,34 @@
+export function objToNode(objNode, insideSvg, options) {
+    let node
+    if (objNode.nodeName === '#text') {
+        node = options.document.createTextNode(objNode.data)
+
+    } else if (objNode.nodeName === '#comment') {
+        node = options.document.createComment(objNode.data)
+    } else {
+        if (objNode.nodeName === 'svg' || insideSvg) {
+            node = options.document.createElementNS('http://www.w3.org/2000/svg', objNode.nodeName)
+            insideSvg = true
+        } else {
+            node = options.document.createElement(objNode.nodeName)
+        }
+        if (objNode.attributes) {
+            Object.entries(objNode.attributes).forEach(([key, value]) => node.setAttribute(key, value))
+        }
+        if (objNode.childNodes) {
+            objNode.childNodes.forEach(childNode => node.appendChild(objToNode(childNode, insideSvg, options)))
+        }
+        if (options.valueDiffing) {
+            if (objNode.value) {
+                node.value = objNode.value
+            }
+            if (objNode.checked) {
+                node.checked = objNode.checked
+            }
+            if (objNode.selected) {
+                node.selected = objNode.selected
+            }
+        }
+    }
+    return node
+}
---
+++ src/diffDOM/virtual/index.js
@@ -0,0 +1,1 @@
+export {DiffFinder} from "./diff"
